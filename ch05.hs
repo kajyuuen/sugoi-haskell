3394857214c40c61f4b45671b8b9f7af6759e42c
Koga Kobayashi <kajyuuen@gmail.com>
2020-06-05 03:19:12 +0900

    Finish p70

--- ch05.hs
+++ ch05.hs
@@ -13,3 +13,34 @@ divideByTen = (/10)
 
 isUpperAlphanum :: Char -> Bool
 isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+-- g(f(a, b): return c, [a], [b]): return [c]
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+-- flip' f = g
+--     where g x y = f y x
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+    | f x = x : filter' f xs
+    | otherwise = filter' f xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = filter (<=x) xs
+        larger = filter (>x) xs
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
